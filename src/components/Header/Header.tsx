96f2e9fd41eb2af37c54af8e8947269ef6cb5921
Alan Lopez <yondaime_97@live.com>
2024-07-07 22:03:23 -0600

    Heading editado

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 export default function Header() {
 	return (
-		<header className="bg-cyan-500">
+		<header className="bg-[#007cba]">
 			<div className="px-5 py-1">
 				<div className="flex justify-between items-center">
 					<div>
@@ -12,19 +12,19 @@ export default function Header() {
 					<nav className="flex gap-6">
 						<NavLink
 							to={'/'}
-							className={({ isActive }) => isActive ? 'text-white uppercase font-bold' : 'text-black uppercase font-bold hover:underline'}
+							className={({ isActive }) => isActive ? 'text-slate-300  uppercase font-bold' : 'taxt-black uppercase font-bold hover:underline'}
 						>Inicio</NavLink>
 						<NavLink
 							to={'services'}
-							className={({ isActive }) => isActive ? 'text-white uppercase font-bold' : 'text-black uppercase font-bold hover:underline'}
+							className={({ isActive }) => isActive ? 'text-slate-300  uppercase font-bold' : 'text-black uppercase font-bold hover:underline'}
 						>Servicios</NavLink>
 						<NavLink
 							to={'prices'}
-							className={({ isActive }) => isActive ? 'text-white uppercase font-bold' : 'text-black uppercase font-bold hover:underline'}
+							className={({ isActive }) => isActive ? 'text-slate-300  uppercase font-bold' : 'text-black uppercase font-bold hover:underline'}
 						>Precios</NavLink>
 						<NavLink
 							to={'contact'}
-							className={({ isActive }) => isActive ? 'text-white uppercase font-bold' : 'text-black uppercase font-bold hover:underline'}
+							className={({ isActive }) => isActive ? 'text-slate-300  uppercase font-bold' : 'text-black uppercase font-bold hover:underline'}
 						>Contacto</NavLink>
 					</nav>
 				</div>
--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -3,8 +3,8 @@ import './Heading.css'
 function Heading() {
 	return (
 		<div className="heading flex items-center py-20 bg-slate-200 gap-12">
-			<div className="text flex flex-col gap-2">
-				<h2 className="text-6xl font-bold">Reparación de dispositivos electrónicos</h2>
+			<div className="text flex flex-col gap-2 mx-auto">
+				<h2 className="text-5xl font-bold">Reparación de dispositivos electrónicos</h2>
 				<p className="text-2xl">
 					Somos expertos en la reparacion de computadoras, tabletas y teléfonos. {''}
 					Ofrecemos un servicio rápido y de alta calidad para mantener tus dispositivos funcionando como nuevos.
